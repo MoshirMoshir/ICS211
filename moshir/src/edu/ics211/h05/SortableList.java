f437180da555aea2c647bd0a3ed44b226fc6bef8
Moshir <alexandermoshir@gmail.com>
2022-02-11 22:59:53 -1000

    Fixed Sorting Methods

--- moshir/src/edu/ics211/h05/SortableList.java
+++ moshir/src/edu/ics211/h05/SortableList.java
@@ -71,8 +71,8 @@ public class SortableList<E> implements ISortableList<E>, IList211<E> {
         // data[nextPos] = table[nextPos - 1]; // Shift down. count the swap
         //data[nextPos] = data[nextPos - 1];
         //data[nextPos - 1] = nextVal;
-        set(indexOf(nextPos), get(nextPos - 1));
-        set(indexOf(nextPos - 1), nextVal);
+        set(nextPos, get(nextPos - 1));
+        set(nextPos - 1, nextVal);
         // nextPos-; // Check next smaller element.
         nextPos--;
       }
@@ -108,8 +108,8 @@ public class SortableList<E> implements ISortableList<E>, IList211<E> {
           E temp = get(j);
           //data [j] = (data[j + 1]);
           //data [j + 1] = temp;
-          set(indexOf(j), get(j + 1));
-          set(indexOf(j + 1), temp);
+          set(j, get(j + 1));
+          set(j + 1, temp);
           // count swap
           numSwaps++;
           // set didSwap true
@@ -170,6 +170,9 @@ public class SortableList<E> implements ISortableList<E>, IList211<E> {
   public E get(int index) {
     // check index
     checkIndex(index);
+    if (index == size) {
+      throw new IndexOutOfBoundsException();
+    }
     // traverse the nodes starting with tail to index
     DLinkedNode temp = traverse(index);
     return temp.item;
@@ -179,6 +182,7 @@ public class SortableList<E> implements ISortableList<E>, IList211<E> {
   // Code from screen cast.
   @Override
   public E set(int index, E element) {
+    /**
     // check the index
     checkIndex(index);
     // traverse to index
@@ -186,6 +190,14 @@ public class SortableList<E> implements ISortableList<E>, IList211<E> {
     // remember the old value
     temp.item = element;
     // return old value
+    return temp.item;
+    */
+    DLinkedNode temp = traverse(index);
+    DLinkedNode node = new DLinkedNode(element, null, null);
+    node.next = temp.next;
+    node.prev = temp.prev;
+    temp.prev.next = temp.next;
+    temp.next.prev = temp.prev;
     return temp.item;
   }
 
@@ -264,6 +276,9 @@ public class SortableList<E> implements ISortableList<E>, IList211<E> {
     checkIndex(index);
     // create a new DLinkedNode with element
     DLinkedNode temp = traverse(index);
+    if (temp == null) {
+      throw new IndexOutOfBoundsException();
+    }
     temp.prev.next = temp.next;
     temp.next.prev = temp.prev;
     size--;
